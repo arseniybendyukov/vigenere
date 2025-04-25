00269ba1e7f1e4b8cfbcc9a00af0ac4c706ed48e
arseniybendyukov <arseniy.bendyukov2006@mail.ru>
2025-04-25 15:18:34 +0200

    text encoding

--- main.py
+++ main.py
@@ -10,17 +10,35 @@ def fill_vigenere_matrix():
     MATRIX = []
     for i in range(len(ALPHABET)):
         MATRIX.append([*ALPHABET[i:], *ALPHABET[:i]])
-       
-       
-fill_vigenere_matrix()
-        
-print(MATRIX)
 
 
-def encode_symbol_vigenere(s1, s2):
-    pass
+def encode_symbol_vigenere(s1: str, s2: str) -> str:
+    '''
+        Returns symbol in uppercase.
+    '''
+    # todo: throw error
+    if s1.upper() not in ALPHABET or s2.upper() not in ALPHABET:
+        return ''
+    i1 = ALPHABET.index(s1.upper())
+    i2 = ALPHABET.index(s2.upper())
+    return MATRIX[i1][i2]
 
 
-s1 = ''
-s2 = ''
-print(encode_symbol_vigenere(s1, s2))
+def encode_text_vigenere(text: str, password: str) -> str:
+    encoded = [*text]
+    for i in range(len(text)):
+        symbol = text[i]
+        if symbol.upper() in ALPHABET:
+            symbol = encode_symbol_vigenere(
+                symbol,
+                password[i % len(password)],
+            )
+        encoded[i] = symbol
+    return ''.join(encoded)
+
+
+if __name__ == '__main__':
+    fill_vigenere_matrix()
+    text = 'ABCD'
+    password = 'C'
+    print(encode_text_vigenere(text, password))
