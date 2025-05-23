3994c665534e8e9ed69d6a8de3c4618a21271983
arseniybendyukov <arseniy.bendyukov2006@mail.ru>
2025-05-23 15:26:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+from typing import List
+
+
+# Der Koinzidenzindex für deutschsprachige Texte
+IC = 0.078
+
+
 # Ä = AE, Ü = UE, Ö = OE
 LETTERS_FREQUENCIES = {
     "E": 0.1740,
@@ -30,7 +37,7 @@ LETTERS_FREQUENCIES = {
 }
 
 
-ALPHABET = 'ABCDEFGHIKLMNOPQRSTUVWXYZẞ'
+ALPHABET = ''.join(sorted(LETTERS_FREQUENCIES.keys()))
 MATRIX = None
 
 
@@ -74,13 +81,45 @@ def encode_text_vigenere(text: str, password: str) -> str:
     return ''.join(encoded)
 
 
-def find_password_length(text: str):
-    pass
+def get_ic(text: str) -> int:
+    '''
+        Gibt den Koinzidenzindex aus.
+        Der Koinzidenzindex ist die Summe von Wahrscheinlichkeiten,
+        dass die zwei zufällig aus dem Text gewählten Symbole gleich sind.
+    '''
+    occurences = {letter: 0 for letter in ALPHABET}
+    for i in range(len(text)):
+        symbol = text[i]
+        if symbol.upper() in ALPHABET:
+            occurences[symbol.upper()] += 1
+    letters_count = sum(occurences.values())
+    return sum([(frequency*(frequency-1) / letters_count*(letters_count-1)) for frequency in occurences.values()])
+    
+
+def get_avg_ic(text: str, groups_count) -> int:
+    '''
+        Gibt den durchschnittlichen Koinzidenzindex von <groups_count> Symbolgruppen aus.
+    '''
+    return sum([get_ic(text[i::groups_count]) for i in range(groups_count)]) / groups_count
+    
+
+def get_best_key_lengths(text: str) -> List[int]:
+    '''
+        Gibt die nach der Abweichung von <IC> sortierte Liste der Längen der Passwörter aus.
+        Die Länge des Passworts muss mindestens 100 Mal kürzer als die Länge des Textes sein.
+        (1 <= Passwort <= Text/100)
+    '''
+    for password_length in range(1, int(len(text) / 100)):
+        # todo
+        pass
+
+
+TEXT = 'Das Leben in der Stadt und auf dem Land unterscheidet sich in vielen Aspekten. In der Stadt gibt es meist mehr Moeglichkeiten: Arbeitsplaetze, Bildungseinrichtungen, kulturelle Angebote und eine bessere Infrastruktur. Viele Menschen schaetzen das Stadtleben wegen seiner Vielfalt und dem schnellen Zugang zu allem, was man im Alltag braucht. Gleichzeitig kann es in der Stadt auch stressig, laut und ueberfuellt sein. Auf dem Land hingegen ist das Leben ruhiger. Die Menschen kennen sich oft untereinander, und die Natur ist meist direkt vor der Haustuer. Besonders Familien mit Kindern oder aeltere Menschen ziehen gerne aufs Land, weil es dort weniger Verkehr, mehr Platz und eine engere Gemeinschaft gibt. Allerdings kann es schwieriger sein, ohne Auto mobil zu bleiben, und viele Dienstleistungen oder Einkaufmoeglichkeiten sind weiter entfernt. Beide Lebensformen haben ihre Vor- und Nachteile. Letztlich haengt die Entscheidung, wo man leben moechte, stark von den persoenlichen Beduerfnissen und Prioritaeten ab. Waehrend manche das pulsierende Leben der Stadt lieben, bevorzugen andere die Ruhe und Naturverbundenheit des Landlebens. Eine ideale Loesung koennte auch eine Mischung aus beidem sein – zum Beispiel in einer kleineren Stadt oder einem gut angebundenen Vorort.'
 
 
 if __name__ == '__main__':
     fill_vigenere_matrix()
-    text = 'Das Leben in der Stadt und auf dem Land unterscheidet sich in vielen Aspekten. In der Stadt gibt es meist mehr Moeglichkeiten: Arbeitsplaetze, Bildungseinrichtungen, kulturelle Angebote und eine bessere Infrastruktur. Viele Menschen schaetzen das Stadtleben wegen seiner Vielfalt und dem schnellen Zugang zu allem, was man im Alltag braucht. Gleichzeitig kann es in der Stadt auch stressig, laut und ueberfuellt sein. Auf dem Land hingegen ist das Leben ruhiger. Die Menschen kennen sich oft untereinander, und die Natur ist meist direkt vor der Haustuer. Besonders Familien mit Kindern oder aeltere Menschen ziehen gerne aufs Land, weil es dort weniger Verkehr, mehr Platz und eine engere Gemeinschaft gibt. Allerdings kann es schwieriger sein, ohne Auto mobil zu bleiben, und viele Dienstleistungen oder Einkaufmoeglichkeiten sind weiter entfernt. Beide Lebensformen haben ihre Vor- und Nachteile. Letztlich haengt die Entscheidung, wo man leben moechte, stark von den persoenlichen Beduerfnissen und Prioritaeten ab. Waehrend manche das pulsierende Leben der Stadt lieben, bevorzugen andere die Ruhe und Naturverbundenheit des Landlebens. Eine ideale Loesung koennte auch eine Mischung aus beidem sein – zum Beispiel in einer kleineren Stadt oder einem gut angebundenen Vorort.'
+    text = TEXT
     password = 'ABCD'
     encoded_text = encode_text_vigenere(text, password)
-    print(encoded_text)
+    print(*range(1, 23.333))
